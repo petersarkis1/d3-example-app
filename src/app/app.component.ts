8f94958552a6826e4532204089c866c3872bde3f
Peter Sarkis <petersarkis@gmail.com>
2024-10-29 11:42:55 -0500

    all the things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'd3-example-app';
+
+  graph: any = {
+    data: {
+      'Series 1': [
+        { x: 1, y: 25 },
+        { x: 2, y: 8 },
+        { x: 4, y: 10 },
+        { x: 5, y: 7 }
+      ],
+      'Series 2': [
+        { x: 1, y: 3 },
+        { x: 2, y: 16 },
+        { x: 3, y: 16 },
+        { x: 6, y: 16 },
+      ],
+      'Series 3': [
+        { x: 1, y: 30 },
+        { x: 2, y: 6 },
+        { x: 3, y: 20 },
+        { x: 4, y: 8 },
+        { x: 5, y: 5 }
+      ]
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TexnetD3Module } from 'texnet-d3-module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TexnetD3Module
   ],
   providers: [],
   bootstrap: [AppComponent]
